63264e48428e47a611c12314f4bbc9ff4bc13c5d
zohaibadnan137 <zohaibadnan137@gmail.com>
2021-12-21 19:32:48 +0500

    Update login screen

--- Aeries/src/module-info.java
+++ Aeries/src/module-info.java
@@ -1,6 +1,9 @@
 module Aeries {
 	requires javafx.controls;
 	requires junit;
+	requires javafx.graphics;
+	requires javafx.fxml;
+	requires javafx.base;
 	
 	opens userinterface to javafx.graphics, javafx.fxml;
 }
