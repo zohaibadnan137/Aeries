ab512c0ea6346ab3a39aec8eb2c0e90b8dcc8146
Huzaifa <mhuzaifa-2000@github.com>
2021-12-21 21:24:03 +0500

    Fixed Few errors

--- Aeries/src/module-info.java
+++ Aeries/src/module-info.java
@@ -1,14 +1,10 @@
 module Aeries {
 	requires javafx.controls;
 	requires junit;
-<<<<<<< HEAD
 	requires java.persistence;
 	requires org.hibernate.orm.core;
-=======
 	requires javafx.graphics;
 	requires javafx.fxml;
 	requires javafx.base;
->>>>>>> branch 'master' of git@github.com:zohaibadnan137/Aeries.git
-	
 	opens userinterface to javafx.graphics, javafx.fxml;
 }
