22bde312ee7ed33e5c99904b5e924309fc51f520
Huzaifa <mhuzaifa-2000@github.com>
2021-12-21 20:49:20 +0500

    sample

--- Aeries/src/businesslogic/Passenger.java
+++ Aeries/src/businesslogic/Passenger.java
@@ -1,12 +1,25 @@
 package businesslogic;
 
+import javax.persistence.*;
+
+
 import utilities.DateAndTime;
 
+@Entity
+@Table(name="Passenger")
 public class Passenger {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	int id;
+	
+	@Column(name="name")
 	private String name;
 	private DateAndTime dateOfBirth;
+	@Column(name="gender")
 	private String gender;
+	@Column(name="address")
 	private String address;
+	@Column(name="passportNumber")
 	private String passportNumber;
 	public Passenger(String name, DateAndTime dateOfBirth, String gender, String address, String passportNumber)
 	{
--- Aeries/src/module-info.java
+++ Aeries/src/module-info.java
@@ -1,6 +1,8 @@
 module Aeries {
 	requires javafx.controls;
 	requires junit;
+	requires java.persistence;
+	requires org.hibernate.orm.core;
 	
 	opens userinterface to javafx.graphics, javafx.fxml;
 }
