38d97a513b7a79848f37cc8fc4d22a14b54a0e5f
Asim <Asim@Asim-PC>
2021-12-21 20:09:52 +0500

    Updated Airline and Testcomplaints

--- Aeries/src/businesslogic/Airline.java
+++ Aeries/src/businesslogic/Airline.java
@@ -5,8 +5,17 @@ public class Airline {
 	
 	
 	private String name;
-	
+	private EmployeeList employeeList;
+	private ComplaintList complaintList;
 	public Airline(String name) {
 		this.name = name;
+	}
+	public Employee authenticateUser(String email,String password)
+	{
+		return employeeList.authenticateUser(email, password);
+	}
+	public void fileComplaint(int complaintID, String description, Passenger passenger)
+	{
+		complaintList.addComplaint(complaintID, description, passenger);
 	}
 }
