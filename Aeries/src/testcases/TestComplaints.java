f0171c97d3e4649ecb8d2875a4c6ce406423fda2
Asim <Asim@Asim-PC>
2021-12-21 12:49:25 +0500

    Updated TestComplaints

--- Aeries/src/testcases/TestComplaints.java
+++ Aeries/src/testcases/TestComplaints.java
@@ -10,15 +10,14 @@ import businesslogic.Passenger;
 import utilities.DateAndTime;
 
 public class TestComplaints {
+
 	@Test
 	public void Test1() {
 		ComplaintList CL=new ComplaintList();
 		DateAndTime d=new DateAndTime(2000, 07, 07, 8, 0, 0);
 		Passenger P=new Passenger("Huzaifa",d,"Male","E9","567849");
-		//Complaint C=new Complaint(1,"Refreshment was not good",P);
-		CL.addComplaint(1, "Refreshment was not good", P);
-		Complaint Cv=new Complaint(1,"Refreshment was not good",P);
-		assertEquals(Cv,CL.getComplaint(1)); 
+		CL.addComplaint(1, "Refreshment was not fine", P);
+		//Complaint Cv=new Complaint(1,"Refreshment was not good",P);
+		assertEquals("Refreshment was not fine",CL.getComplaint(1).getDescription()); 
 	}
-	
 }
