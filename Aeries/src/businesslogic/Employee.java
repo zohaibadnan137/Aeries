2e6cfc9bbb36d94228b45f0959c3779f4232d387
Huzaifa <mhuzaifa-2000@github.com>
2021-12-22 23:45:34 +0500

    added emp getters

--- Aeries/src/businesslogic/Employee.java
+++ Aeries/src/businesslogic/Employee.java
@@ -41,5 +41,42 @@ public class Employee {
 	{
 		this.position = position;
 	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public DateAndTime getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(DateAndTime dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 	//Dummy comment
 }
