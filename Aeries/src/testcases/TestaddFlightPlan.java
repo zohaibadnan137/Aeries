4f3f7c9e80287acba956f4c7779cbdd0fb5db77c
Asim <Asim@Asim-PC>
2021-12-23 02:28:02 +0500

    TestAddFlightPlan

---
+++ Aeries/src/testcases/TestaddFlightPlan.java
@@ -0,0 +1,15 @@
+package testcases;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import businesslogic.Airline;
+import businesslogic.EmployeeList;
+import utilities.DateAndTime;
+
+public class TestaddFlightPlan {
+	@Test
+	public void Test1() {
+		
+	}
+}
