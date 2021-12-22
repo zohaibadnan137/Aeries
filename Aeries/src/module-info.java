6c72876a2d46280ae3224d16c9013b63c6112197
zohaibadnan137 <zohaibadnan137@gmail.com>
2021-12-22 18:13:26 +0500

    Add main menu and welcome screen

--- Aeries/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module Aeries {
-	requires javafx.controls;
-	requires junit;
-	requires javafx.graphics;
-	requires javafx.fxml;
-	requires javafx.base;
-	
-	opens userinterface to javafx.graphics, javafx.fxml;
-}
---
+++ Aeries/src/userinterface/LoginEventHandler.java
@@ -0,0 +1,46 @@
+package userinterface;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class LoginEventHandler {
+	
+	private String username;
+	private String password;
+	
+    @FXML
+    private PasswordField passwordInput;
+    @FXML
+    private TextField usernameInput;
+    
+    @FXML
+    private Label invalidCredentials;
+    
+    @FXML
+    private void start(MouseEvent event)
+    {
+		invalidCredentials.setText("");
+    }
+	
+	@FXML
+	private void authenticate(ActionEvent event)
+	{
+		boolean success = false;
+		//success = Airline.authenticate(usernameInput.getText(), passwordInput.getText());
+		if(success)
+		{
+			// Call main menu
+		}
+		else
+		{
+			usernameInput.setText("");
+			passwordInput.setText("");
+			invalidCredentials.setText("Invalid credentials. Please try again.");
+		}
+	}
+
+}
