c40882b455aa97a350dcdbceaa30bdb8fa06e08d
Huzaifa <mhuzaifa-2000@github.com>
2021-12-22 14:17:07 +0500

    Implemented Print Ticket

--- Aeries/src/businesslogic/Airline.java
+++ Aeries/src/businesslogic/Airline.java
@@ -5,9 +5,59 @@ public class Airline {
 	private String name;
 	private EmployeeList employeeList;
 	private ComplaintList complaintList;
-	public Airline(String name) {
+	private FlightPlanCatalog flightPlanCatalog;
+	private FlightSchedule flightSchedule;
+	private Fleet fleet;
+	public Airline() {
+		this.name = "";
+		this.employeeList = null;
+		this.complaintList = null;
+		this.flightPlanCatalog = null;
+		this.flightSchedule = null;
+		this.fleet = null;
+	}
+	public Airline(String name,EmployeeList employeeList,ComplaintList complaintList,FlightPlanCatalog flightPlanCatalog, FlightSchedule flightSchedule,Fleet fleet) {
+		this.name = name;
+		this.employeeList = employeeList;
+		this.complaintList = complaintList;
+		this.flightPlanCatalog = flightPlanCatalog;
+		this.flightSchedule = flightSchedule;
+		this.fleet = fleet;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
 		this.name = name;
 	}
+	public EmployeeList getEmployeeList() {
+		return employeeList;
+	}
+	public void setEmployeeList(EmployeeList employeeList) {
+		this.employeeList = employeeList;
+	}
+	public FlightPlanCatalog getFlightPlanCatalog() {
+		return flightPlanCatalog;
+	}
+	public void setFlightPlanCatalog(FlightPlanCatalog flightPlanCatalog) {
+		this.flightPlanCatalog = flightPlanCatalog;
+	}
+	public FlightSchedule getFlightSchedule() {
+		return flightSchedule;
+	}
+	public void setFlightSchedule(FlightSchedule flightSchedule) {
+		this.flightSchedule = flightSchedule;
+	}
+	public Fleet getFleet() {
+		return fleet;
+	}
+	public void setFleet(Fleet fleet) {
+		this.fleet = fleet;
+	}
+	public void setComplaintList(ComplaintList complaintList) {
+		this.complaintList = complaintList;
+	}
+	
 	public Employee authenticateUser(String email,String password)
 	{
 		return employeeList.authenticateUser(email, password);
@@ -20,4 +70,5 @@ public class Airline {
 	{
 		return this.complaintList;
 	}
+	public void addFlight()
 }
