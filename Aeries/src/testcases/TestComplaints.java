d340474aa1b030233005cdaa4f30f9f4bdad0390
Asim <Asim@Asim-PC>
2021-12-24 22:38:01 +0500

    jo bhi ha

--- Aeries/src/testcases/TestComplaints.java
+++ Aeries/src/testcases/TestComplaints.java
@@ -2,11 +2,15 @@ package testcases;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import org.junit.Test;
 
 import businesslogic.Airline;
 import businesslogic.BoardingPass;
+import businesslogic.Complaint;
 import businesslogic.ComplaintList;
 import businesslogic.Passenger;
 import utilities.DateAndTime;
@@ -14,18 +18,19 @@ import utilities.DateAndTime;
 public class TestComplaints {
 
 	@Test
-	public void Test1() {
+	public void Test1() throws FileNotFoundException, IOException {
 		ComplaintList CL=new ComplaintList();
 		Airline A=new Airline("Aeries", null, CL, null, null, null);
 		DateAndTime d=new DateAndTime(2000, 07, 07, 8, 0, 0);
 		Passenger P=new Passenger("Huzaifa",d,"Male","E9","567849");
 		BoardingPass pass=new BoardingPass(12, null, P, null);
-		A.fileComplaint(1, "Refreshment was not fine", pass);
+		A.fileComplaint("Refreshment was not fine", pass);
 		CL=A.getComplaintList();
-		assertEquals("Huzaifa",CL.getComplaint(1).getBoardingPass().getAssignedPassenger().getName()); 
+		ArrayList<Complaint> CLL=CL.getComplaints();
+		assertEquals("Huzaifa",CLL.get(0).getBoardingPass().getAssignedPassenger().getName()); 
 	}
 	@Test
-	public void Test2() {
+	public void Test2() throws FileNotFoundException, IOException {
 		ComplaintList CL=new ComplaintList();
 		Airline A=new Airline("Aeries", null, CL, null, null, null);
 		DateAndTime d1=new DateAndTime(2000, 8, 07, 8, 0, 0);
@@ -37,17 +42,24 @@ public class TestComplaints {
 		BoardingPass pass1=new BoardingPass(12, null, P1, null);
 		BoardingPass pass2=new BoardingPass(12, null, P2, null);
 		BoardingPass pass3=new BoardingPass(12, null, P3, null);
-		A.fileComplaint(1, "Refreshment was not good", pass1);
-		A.fileComplaint(2, "Temperature was not as it should be.", pass1);
-		A.fileComplaint(3, "Washroom was not cleaned.", pass1);
-		A.fileComplaint(4, "Food Quality was kinda low", pass2);
-		A.fileComplaint(5, "Air hostess was not cooperative", pass2);
-		A.fileComplaint(6, "Luggage got damaged during loading", pass3);
-		A.fileComplaint(7, "there was error in tabs", pass3);
+		A.fileComplaint("Refreshment was not good", pass1);
+		A.fileComplaint("Temperature was not as it should be.", pass1);
+		A.fileComplaint("Washroom was not cleaned.", pass1);
+		A.fileComplaint("Food Quality was kinda low", pass2);
+		A.fileComplaint("Air hostess was not cooperative", pass2);
+		A.fileComplaint("Luggage got damaged during loading", pass3);
+		A.fileComplaint("there was error in tabs", pass3);
 		CL=A.getComplaintList();
-		assertEquals("Refreshment was not good",CL.getComplaint(1).getDescription()); 
-		assertEquals("876563",CL.getComplaint(4).getBoardingPass().getAssignedPassenger().getPassportNumber());
-		assertEquals("g13",CL.getComplaint(6).getBoardingPass().getAssignedPassenger().getAddress());		
+		BoardingPass pp=null;
+		ArrayList<Complaint> CLL=CL.getComplaints();
+		for(int i=0;i<CLL.size();i++)
+		{
+			if(CLL.get(i).getDescription().equals("Refreshment was not good"))
+			{
+				pp=CLL.get(i).getBoardingPass();
+			}
+		}
+		assertEquals("Huzaifa",pp.getAssignedPassenger().getName()); 
 	}
 	
 }
